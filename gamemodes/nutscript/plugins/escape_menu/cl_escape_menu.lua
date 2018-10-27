90b396ee1e3a558d4a2a30a111a63ae170fb90cd
Oskar <saturdaysheroes.business@gmail.com>
2018-10-27 14:24:22 +0200

    dodanie fontów

--- gamemodes/nutscript/plugins/escape_menu/cl_escape_menu.lua
+++ gamemodes/nutscript/plugins/escape_menu/cl_escape_menu.lua
@@ -1,5 +1,49 @@
 if !CLIENT then return end 
 
+local fonttext = "Arial"
+
+	surface.CreateFont( "text",
+	{
+		font      = fonttext,
+		size      = 24,
+		weight    = -1,
+	}
+
+ )
+	surface.CreateFont( "text_blur",
+	{
+		font      = fonttext,
+		size      = 24,
+		weight    = -1,
+		underline = 0,
+		additive  = false,
+		outline = false,
+		blursize = 6
+	}
+
+ )
+
+surface.CreateFont( "text_big",
+	{
+		font      = fonttext,
+		size      = 72,
+		weight    = -1,
+	}
+
+ )
+	surface.CreateFont( "text_blur_big",
+	{
+		font      = fonttext,
+		size      = 72,
+		weight    = -1,
+		underline = 0,
+		additive  = false,
+		outline = false,
+		blursize = 10
+	}
+
+ )
+
 function Esc.AddBatton( Button )
 	FalloutTable[#FalloutTable + 1] = Button
 end
