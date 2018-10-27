04d658f97486b70072a48cc657595746246abb6a
Lechu2375 <adamos2375@gmail.com>
2018-10-27 20:58:22 +0200

    Dodano overlay'a z logiem sieci.
    Overlay z logiem sieci.

--- gamemodes/falloutrp/schema/hooks/sv_hooks.lua
+++ gamemodes/falloutrp/schema/hooks/sv_hooks.lua
@@ -11,3 +11,16 @@ function SCHEMA:PlayerFootstep(client, position, foot, soundName, volume)
 		end
 	end
 end
+
+--------------------OVERLAY-------------------------------------------
+local firestone_overlay = Material( "firestone_overlay.png" )
+hook.Add( "HUDPaint", "example_hookseeses", function()
+	surface.SetDrawColor( 255, 255, 255, 220 )
+	surface.SetMaterial( firestone_overlay ) 
+	surface.DrawTexturedRect( 0, 0, 350, 100 )
+end )
+----------------------------------------------------------------------
+
+
+
+
