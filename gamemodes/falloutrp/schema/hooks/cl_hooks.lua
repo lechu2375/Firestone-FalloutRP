79fae5f74724ef1690c5046f78ef52db6268c3fe
Mateusz <wiossna@spoko.pl>
2018-10-27 21:58:56 +0200

    Added scaling, fixed code

--- gamemodes/falloutrp/schema/hooks/cl_hooks.lua
+++ gamemodes/falloutrp/schema/hooks/cl_hooks.lua
@@ -1,13 +1,15 @@
-------------------OVERLAY LOGO SIECI-----------------------------
-resource.AddFile( "materials/fs_overlay/picture.png" 
+if !CLIENT then return end
+
+------------------OVERLAY LOGO SIECI---------------------------
+
+resource.AddFile("materials/fs_overlay/firestrone_overlay.png")
 local firestone_overlay = Material( "fs_overlay/firestone_overlay.png" )
+local scale = ScrH()
+
 hook.Add( "HUDPaint", "fs_logo_overlay", function()
 	surface.SetDrawColor( 255, 255, 255, 220 )
-	surface.SetMaterial( firestone_overlay ) 
-	surface.DrawTexturedRect( 0, 0, 350, 100 )
+	surface.SetMaterial( firestone_overlay )
+	surface.DrawTexturedRect( ScrW() - scale*0.23, 10, scale*0.23, scale*0.08 )
 end )
+
 ---------------------------------------------------------------------
-
-
-
-
