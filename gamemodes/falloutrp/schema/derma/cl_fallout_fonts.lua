a68d6ccd85cd88de5f8779df8654c5c73c11e02f
Mateusz <wiossna@spoko.pl>
2018-10-27 22:14:28 +0200

    Dodano plik z czcionkami

---
+++ gamemodes/falloutrp/schema/derma/cl_fallout_fonts.lua
@@ -0,0 +1,26 @@
+resource.AddFile("resources/fonts/BebasNeue_regular.ttf")
+
+
+surface.CreateFont( "Medium_Fallout_Font", {
+	font = "Bebas Neue Regular",
+	size = 26,
+	antialias = true,
+} )
+
+surface.CreateFont( "Medium_Fallout_Font_Bold", {
+	font = "Bebas Neue Bold",
+	size = 26,
+	antialias = true,
+} )
+
+surface.CreateFont( "Medium_Overseer_Font", {
+	font = "Overseer",
+	size = 26,
+	antialias = true,
+} )
+
+surface.CreateFont( "Medium_Overseer_Font_Bold", {
+	font = "Overseer Bold",
+	size = 26,
+	antialias = true,
+} )
