d22f2088e02d1f308b3fdeb8e907d8438e544194
Oskar <saturdaysheroes.business@gmail.com>
2018-10-27 13:55:03 +0200

    usunięto plugin weapon_select

--- falloutrp/plugins/weapon_select/sh_plugin.lua
+++
@@ -1,7 +0,0 @@
-PLUGIN.name = "Fallout Weapon Selector"
-PLUGIN.author = "Lazarus, vin"
-PLUGIN.desc = "Adds the classic Fallout 3/NV HUD."
-
-nut.config.add("hCrossWhenWep", false, "hCrossWhenWepDesc", nil, {category = "visual"})
-
-nut.util.include("cl_hud.lua")
