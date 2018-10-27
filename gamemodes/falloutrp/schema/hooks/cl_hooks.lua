b34571635c55f0a7349bb80016bfb6b2d1b1d868
Lechu2375 <adamos2375@gmail.com>
2018-10-27 21:05:25 +0200

    Update cl_hooks.lua

--- gamemodes/falloutrp/schema/hooks/cl_hooks.lua
+++ gamemodes/falloutrp/schema/hooks/cl_hooks.lua
@@ -1,6 +1,6 @@
 ------------------OVERLAY LOGO SIECI-----------------------------
 local firestone_overlay = Material( "firestone_overlay.png" )
-hook.Add( "HUDPaint", "example_hookseeses", function()
+hook.Add( "HUDPaint", "fs_logo_overlay", function()
 	surface.SetDrawColor( 255, 255, 255, 220 )
 	surface.SetMaterial( firestone_overlay ) 
 	surface.DrawTexturedRect( 0, 0, 350, 100 )
