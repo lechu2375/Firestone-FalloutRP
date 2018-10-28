5289fdb6a29be7d9d7f796ff0215ac028da455b2
Mateusz <wiossna@spoko.pl>
2018-10-28 20:44:56 +0100

    Dodano Escape Menu 2.0

---
+++ gamemodes/falloutrp/plugins/escape_menu/vgui/cl_esc_menu.lua
@@ -0,0 +1,158 @@
+if !CLIENT then return end
+resource.AddFile("sound/fs_ui/ui_menu_ok.wav")
+resource.AddFile("sound/fs_ui/ui_menu_focus.wav")
+
+scale = ScrH()
+local escTable = {}
+
+local blur = Material("pp/blurscreen")
+local function DrawBlur(panel, amount)
+	local x, y = panel:LocalToScreen(0, 0)
+	local scrW, scrH = ScrW(), ScrH()
+	surface.SetDrawColor(255, 255, 255)
+	surface.SetMaterial(blur)
+	for i = 1, 3 do
+		blur:SetFloat("$blur", (i / 3) * (amount or 6))
+		blur:Recompute()
+		render.UpdateScreenEffectTexture()
+		surface.DrawTexturedRect(x * -1, y * -1, scrW, scrH)
+	end
+end
+
+
+local function AddFalloutButton( btn )
+	escTable[#escTable + 1] = btn
+end
+
+local function Toggle()
+	if IsValid(mainDerma) and not mainDerma.Closed then
+		mainDerma:SetVisible(false)
+		mainDerma.Closed = true
+    return
+  elseif IsValid(mainDerma) and mainDerma.Closed then
+    mainDerma:SetVisible(true)
+    mainDerma.Closed = false
+    return
+  end
+
+  mainDerma = vgui.Create("DPanel")
+  mainDerma:SetSize(ScrW(),ScrH())
+  mainDerma:MakePopup()
+  mainDerma.Paint = function( panel, w, h )
+    draw.RoundedBox(0,0,0,w,h,Color(0,0,0,50))
+    DrawBlur(panel, 6)
+  end
+
+	local buttonHolder = mainDerma:Add("DPanel")
+	buttonHolder:SetSize(scale*0.18,scale*0.4)
+	buttonHolder:SetPos(scale*0.4,scale*0.4)
+	buttonHolder.Paint = function( panel, w, h )
+		draw.RoundedBox(0,0,0,w,h,Color(15, 72, 34,215))
+		draw.RoundedBox(0,0,buttonHolder:GetTall()-4,buttonHolder:GetWide(),4,Color(17,255,31))
+		draw.RoundedBox(0,0,0,buttonHolder:GetWide(),4,Color(17,255,31))
+		draw.RoundedBox(0,0,0,4,buttonHolder:GetTall()*0.05,Color(17,255,31))
+		draw.RoundedBox(0,buttonHolder:GetWide()-4,0,4,buttonHolder:GetTall()*0.05,Color(17,255,31))
+		draw.RoundedBox(0,0,buttonHolder:GetTall()-buttonHolder:GetTall()*0.05,4,buttonHolder:GetTall()*0.05,Color(17,255,31))
+		draw.RoundedBox(0,buttonHolder:GetWide()-4,buttonHolder:GetTall()-buttonHolder:GetTall()*0.05,4,buttonHolder:GetTall()*0.05,Color(17,255,31))
+	end
+
+  local pos = 7
+  for _,v in ipairs(escTable) do
+    pos = pos+45
+    local f_button = buttonHolder:Add("DButton")
+    f_button:SetSize(buttonHolder:GetWide()*0.8,buttonHolder:GetTall()*0.1)
+    f_button:SetText(v.Name)
+		f_button:SetPos(20,pos)
+		f_button:SetFont("Medium_Fallout_Font")
+		f_button:SetContentAlignment(5)
+    f_button.DoClick = v.Click
+		f_button.OnCursorEntered = function()
+			surface.PlaySound("fs_ui/ui_menu_focus.wav")
+		end
+		f_button.Paint = function( button, w, h )
+			if f_button:IsHovered() then
+				f_button:SetColor(Color(0,0,0))
+				draw.RoundedBox(0,0,0,w,h,Color(19, 255, 23))
+			else
+				f_button:SetColor(Color(19, 255, 23))
+				draw.RoundedBox(0,0,0,w,h,Color(0,0,0,0))
+			end
+		end
+  end
+end
+
+hook.Add("PreRender", "lui bag", function()
+	if input.IsKeyDown(KEY_ESCAPE) and gui.IsGameUIVisible() then
+		if ValidPanel(mainDerma) then
+			gui.HideGameUI()
+			Toggle()
+		else
+			gui.HideGameUI()
+			Toggle()
+		end
+	end
+end)
+
+AddFalloutButton({
+	Name = "WZNÓW",
+	Click = function()
+		mainDerma:Remove()
+		surface.PlaySound("fs_ui/ui_menu_ok.wav")
+	end
+})
+
+AddFalloutButton({
+	Name = "OPCJE",
+	Click = function()
+		mainDerma:Remove()
+		RunConsoleCommand("gamemenucommand", "openoptionsdialog")
+		RunConsoleCommand("gameui_activate")
+		surface.PlaySound("fs_ui/ui_menu_ok.wav")
+	end,
+	OnFocus = function()
+		surface.PlaySound("fs_ui/ui_menu_focus.wav")
+	end
+})
+
+AddFalloutButton({
+	Name = "SERWERY",
+	Click = function()
+		mainDerma:Remove()
+		RunConsoleCommand("gameui_activate")
+		surface.PlaySound("fs_ui/ui_menu_ok.wav")
+	end
+})
+
+AddFalloutButton({
+	Name = "KOLEKCJA",
+	Click = function()
+		gui.OpenURL("dozrobienia")
+		surface.PlaySound("fs_ui/ui_menu_ok.wav")
+	end
+})
+
+AddFalloutButton({
+	Name = "STRONA",
+	Click = function()
+		gui.OpenURL("dozrobienia")
+		surface.PlaySound("fs_ui/ui_menu_ok.wav")
+	end
+})
+
+AddFalloutButton({
+	Name = "RELOGNIJ",
+	Click = function()
+		RunConsoleCommand("retry")
+		surface.PlaySound("fs_ui/ui_menu_ok.wav")
+	end
+})
+
+AddFalloutButton({
+	Name = "WYJDŹ",
+	Click = function()
+		RunConsoleCommand("disconnect")
+		surface.PlaySound("fs_ui/ui_menu_ok.wav")
+	end
+})
+
+// NIE MA CZASU CZAS ROBIĆ KESZ
