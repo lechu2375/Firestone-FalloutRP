f8f2390820aaa7c0a1aa8955c377cb8607bc8901
Oskar <saturdaysheroes.business@gmail.com>
2018-10-29 22:03:00 +0100

    stworzono pierwszy element vgui (FS_Panel)

--- gamemodes/falloutrp/schema/derma/cl_panel.lua
+++ gamemodes/falloutrp/schema/derma/cl_panel.lua
@@ -2,10 +2,13 @@ AddCSLuaFile()
 
 local PANEL = {}
 
+local blur = Material("pp/blurscreen")
+
 function PANEL:Init()
   self.dark = Color(15, 72, 34,215)
   self.light = Color(17,255,31)
   self:MakePopup()
+  self:SetTitle("")
 end
 
 function PANEL:SetTitle(strTitle)
@@ -15,7 +18,24 @@ function PANEL:SetTitle(strTitle)
 end
 
 function PANEL:Paint(w, h)
+  local x, y = self:LocalToScreen(0, 0)
+    local scrW, scrH = ScrW(), ScrH()
+    surface.SetDrawColor(255, 255, 255)
+    surface.SetMaterial(blur)
+    for i = 1, 1.1 do
+        blur:SetFloat("$blur", (i / 3) * (amount or 6))
+        blur:Recompute()
+        render.UpdateScreenEffectTexture()
+        surface.DrawTexturedRect(x * -1, y * -1, scrW, scrH)
+    end
   draw.RoundedBox(0, 0, 0, w, h, self.dark)
+  surface.SetDrawColor(self.light)
+  surface.DrawRect(0,self:GetTall()-4,self:GetWide(),4)
+  surface.DrawRect(0,0,self:GetWide(),4)
+  surface.DrawRect(0,0,4,self:GetTall()*0.02)
+  surface.DrawRect(self:GetWide()-4,0,4,self:GetTall()*0.02)
+  surface.DrawRect(0,self:GetTall()-self:GetTall()*0.02,4,self:GetTall()*0.02)
+  surface.DrawRect(self:GetWide()-4,self:GetTall()-self:GetTall()*0.02,4,self:GetTall()*0.02)
 end
 
-vgui.Register("FS_Panel", PANEL, "DFrame")
+vgui.Register("FS_Panel", PANEL, "DPanel")
