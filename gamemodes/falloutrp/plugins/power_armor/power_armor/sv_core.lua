c9fc10a120f3a9c3a7e1134865870ae4e8b4c253
Oskar <saturdaysheroes.business@gmail.com>
2018-11-03 20:00:48 +0100

    naprawiono dzwięk chodzenia + dodano precache na sound

--- gamemodes/falloutrp/plugins/power_armor/power_armor/sv_core.lua
+++ gamemodes/falloutrp/plugins/power_armor/power_armor/sv_core.lua
@@ -1,6 +1,19 @@
 local PLUGIN = PLUGIN
 local PLAYER = FindMetaTable("Player")
 
+local sounds = {
+    "fs_power_01",
+    "fs_power_02",
+    "fs_power_03",
+    "fs_power_04",
+    "fs_power_05",
+    "fs_power_06"
+}
+
+for _, v in ipairs(sounds) do
+    util.PrecacheSound(v)
+end
+
 function PLAYER:SetPowerArmor(bState)
     if bState == true then 
         self:SetNWBool("Firestone.PowerArmor", true)
@@ -21,7 +34,7 @@ end
 
 function PLUGIN:PlayerFootstep(ply, pos, foot, sound, volume, rf)
     if ply:GetPowerArmor() == true then 
-        ply:EmitSound("fs_armor/fs_power_0"..math.random(1, 6)..".wav", 75, 100, CHAN_AUTO)
+        ply:EmitSound("fs_armor/fs_power_0"..math.random(1, 6)..".wav", 75, 100, 1, CHAN_AUTO)
         return true
     else
         return false
