1a50f2a03b4fd40a0d9491a2508cddec14b4e934
Oskar <saturdaysheroes.business@gmail.com>
2018-11-04 21:45:42 +0100

    dodano backend bug menu (do testu)

--- gamemodes/falloutrp/plugins/bug_menu/core/sv_core.lua
+++ gamemodes/falloutrp/plugins/bug_menu/core/sv_core.lua
@@ -12,4 +12,58 @@ PLUGIN:PlayerSay(ply, text)
     end
 end
 
+net.Receive("fs_bug_info", function(len, ply)
+    local problem = net.ReadString()
+    local priority = net.ReadInt()
+    local priority_color 
+    local priority_name
+
+    if priority == 1 then 
+        priority_color = 6729778
+        priority_name = "Niski"
+    end
+
+    if priority == 2 then 
+        priority_color = 16753920
+        priority_name = "Średni"
+    end
+
+    if priority == 3 then 
+        priority_color = 16711680
+        priority_name = "Wysoki"
+    end
+
+
+    local payload = {
+        embeds = {
+            {
+                fields = {
+                    {
+                        name = "Zgłaszajacy",
+                        value = "Nick:"..ply:Nick().."\n Steam ID:"..ply:SteamID64().."\n Ranga:"..ply:GetUserGroup().."",
+                        url = "https://steamcommunity.com/profiles/"..ply:SteamID64().."/",
+                    },
+                    {
+                        name = "Opis",
+                        value = problem,
+                        inline = false,
+                    },
+                    {
+                        name = "Priorytet",
+                        value = priority_name,
+                        inline = false,
+                    },
+                },
+                thumbnail = {
+                    url = "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/43/4396047dea902e3538d281a47d1742ebc1c6ad87_full.jpg"
+                },
+
+                color = priority_color
+            }
+        }
+    }
+
+    http.Post(PLUGIN.WebhookURL, {payload_json = util.TableToJSON(payload)})
+    
+end)
 
