475a31559cce36b17b82dc2032ca6692d06d3282
Lechu2375 <adamos2375@gmail.com>
2018-11-04 16:41:15 +0100

    Modele dla rang
    Przypisano model dla każdej rangi

--- gamemodes/falloutrp/schema/sh_config.lua
+++ gamemodes/falloutrp/schema/sh_config.lua
@@ -8,3 +8,17 @@ SCHEMA.runRanks = {""}
 --[Bractwo Stali]----
 
 SCHEMA.bosRanks = {"Nowicjusz", "Kadet", "Rycerz",  "Rycerz Sierżant", "Rycerz Kapitan", "Starszy Rycerz", "Paladyn", "Starszy Paladyn", "Gwieździsty Paladyn", "Wartownik", "Starszy"}
+
+SCHEMA.bosModels = {
+	["Nowicjusz"] = "models/reconarmor/reconarmor_01.mdl"
+	["Kadet"] = "models/reconarmor/reconarmor_01.mdl"
+	["Rycerz"] = "models/hub/fallout/armors/knight.mdl"
+	["Rycerz Sierżant"] = "models/hub/fallout/armors/knight.mdl"
+	["Rycerz Kapitan"] = "models/hub/fallout/armors/knight.mdl"
+	["Starszy Rycerz"] = "models/hub/fallout/armors/knight.mdl"
+	["Paladyn"] = "models/hub/fallout/armors/paladin.mdl"
+	["Starszy Paladyn"] = "models/hub/fallout/armors/starpaladin.mdl"
+	["Gwieździsty Paladyn"] = "models/hub/fallout/armors/starpaladin.mdl"
+	["Wartownik"] = "models/hub/fallout/armors/starpaladin.mdl"
+	["Starszy"] = "models/hub/armors/midwestpaladin.mdl"
+}
