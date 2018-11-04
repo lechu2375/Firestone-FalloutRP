2194f2255174a63b6deceb669b4d9c58129c3813
Lechu2375 <adamos2375@gmail.com>
2018-11-04 16:25:55 +0100

    Rangi Bractwa
    Dodano Rangi Bractwa

--- gamemodes/falloutrp/schema/sh_config.lua
+++ gamemodes/falloutrp/schema/sh_config.lua
@@ -2,3 +2,9 @@ SCHEMA.digitsLen = 4
 
 SCHEMA.runPrefix = "TESTER"
 SCHEMA.runRanks = {""}
+
+
+
+--[Bractwo Stali]----
+
+SCHEMA.bosRanks = {"Nowicjusz", "Kadet", "Rycerz",  "Rycerz Sierżant", "Rycerz Kapitan", "Starszy Rycerz", "Paladyn", "Starszy Paladyn", "Gwieździsty Paladyn", "Wartownik", "Starszy"}
