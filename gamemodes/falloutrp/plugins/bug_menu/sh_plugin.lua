9b0331eb4e82bb8b1e1095001c8373f99cb8a65c
Oskar <saturdaysheroes.business@gmail.com>
2018-11-04 21:29:57 +0100

    dodano PLUGIN.WebhookURL dla bug menu

--- gamemodes/falloutrp/plugins/bug_menu/sh_plugin.lua
+++ gamemodes/falloutrp/plugins/bug_menu/sh_plugin.lua
@@ -2,5 +2,7 @@ local PLUGIN = PLUGIN
 PLUGIN.name = "Bug Menu"
 PLUGIN.author = "SaturdaysHeroes & Djuk"
 
+PLUGIN.WebhookURL = "https://discordapp.com/api/webhooks/508739433202188335/WJBuEEla0fZegVrfh92PeFCJfxhvm4-zGjJcAXzT_L5-zN-SriTKWC2TusrGdPWQUe35"
+
 local dir = PLUGIN.folder.."/"
 nut.util.includeDir(dir.."core", true, true)
