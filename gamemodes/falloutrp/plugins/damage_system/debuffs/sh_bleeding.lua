75d43a049280a558791e3c5d62ad4a8e5d644951
FrX <35167464+Frxxx@users.noreply.github.com>
2018-11-06 21:27:33 +0100

    Rework krwawienia.

--- gamemodes/falloutrp/plugins/damage_system/debuffs/sh_bleeding.lua
+++ gamemodes/falloutrp/plugins/damage_system/debuffs/sh_bleeding.lua
@@ -2,8 +2,13 @@ local PLUGIN = PLUGIN
 local PLAYER = FindMetaTable("Player")
 
 DamageSys.BleedingInterval = 3
+DamageSys.BleedingBodyParts = DamageSys.BleedingBodyParts or {}
 
-function PLAYER:ApplyBleeding(intAmount)
+function PLAYER:ApplyBleeding(intAmount, strBodyPart)
+    self:SetNWInt("Firestone."..strBodyPart..".Bleeding", self:GetNWInt("Firestone."..strBodyPart..".Bleeding", 0) + intAmount)
+    if !table.HasValue(DamageSys.BleedingBodyParts, strBodyPart) then
+        table.insert(DamageSys.BleedingBodyParts, strBodyPart)
+    end
     self:SetNWInt("Firestone.Bleeding", self:GetNWInt("Firestone.Bleeding", 0) + intAmount)
     self.BleedingTimer = CurTime()
 end
