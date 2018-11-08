69c815c685559c4ae3d0e029e8dd61db3d9eec81
Mateusz <wiossna@spoko.pl>
2018-11-08 20:37:00 +0100

    Hotfix rangi Prime

--- gamemodes/falloutrp/plugins/prime/sh_plugin.lua
+++ gamemodes/falloutrp/plugins/prime/sh_plugin.lua
@@ -1,10 +1,10 @@
-local PLUGIN = PLUGIN 
+local PLUGIN = PLUGIN
 
 PLUGIN.name = "Prime Module"
 PLUGIN.author = "SaturdaysHeroes"
 
 function PLUGIN:GetMaxPlayerCharacter(client)
-    if table.HasValue({"prime", "early_supporter"}, client:GetUserGroup()) then 
+    if table.HasValue({"prime", "early_supporter"}, client:GetNWInt("usergroup")) then 
         return nut.config.get("maxChars") + 1
-    end 
+    end
 end
