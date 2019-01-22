34d8aae475875ae30f12c3e26ce703e506b40211
FrX_ <35167464+Frxxx@users.noreply.github.com>
2019-01-22 21:47:01 +0100

    Usuwanko
    W złe miejsce wrzuciłem plugin x-D

--- damage_system/debuffs/sh_bw.lua
+++
@@ -1,8 +0,0 @@
-local PLAYER = FindMetaTable("Player")
-
-function PLAYER:BW(intLength)
-    local char = self:getChar()
-    self:setRagdolled(true, intLength)
-end
-
--- łeb mnie rozbolał i już mi się nie chce ok
