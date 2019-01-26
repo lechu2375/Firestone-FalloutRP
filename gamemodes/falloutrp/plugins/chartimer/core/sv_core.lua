1c66111baa13ad701d12e6d6abfc77827f4fcacd
Adam <adamos2375@gmail.com>
2019-01-26 17:45:16 +0100

    Power rangers init

--- gamemodes/falloutrp/plugins/chartimer/core/sv_core.lua
+++ gamemodes/falloutrp/plugins/chartimer/core/sv_core.lua
@@ -1,16 +1,17 @@
 if !SERVER then return end 
 
 local PLUGIN = PLUGIN
-
-if timer.Exists("chartimer") then return end
-timer.Create("chartimer", 30, 0, function()
-    for _,v in pairs(player.GetAll()) do
-    local char = v:getChar()
-    if !char then return end
-        if (!char:getData("chartime") or char:getData("chartime") == nil)  then
-            char:setData("chartime", 30)
-        else
-            char:setData("chartime", char:getData("chartime") + 30)
+function PLUGIN:Initialize()
+    if timer.Exists("chartimer") then return end
+    timer.Create("chartimer", 30, 0, function()
+        for _,v in pairs(player.GetAll()) do
+        local char = v:getChar()
+        if !char then return end
+            if (!char:getData("chartime") or char:getData("chartime") == nil)  then
+                char:setData("chartime", 30)
+            else
+                char:setData("chartime", char:getData("chartime") + 30)
+            end
         end
-    end
-end)
+    end)
+end
