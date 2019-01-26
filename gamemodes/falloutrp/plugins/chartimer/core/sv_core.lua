ab2a2c86c0b6f7223b2c1918929c8487be2eb84b
Adam <adamos2375@gmail.com>
2019-01-26 17:34:43 +0100

    local mial byc
    no kurwa global vara zrobilem xdd
    przepraszam za przeklenstwo

--- gamemodes/falloutrp/plugins/chartimer/core/sv_core.lua
+++ gamemodes/falloutrp/plugins/chartimer/core/sv_core.lua
@@ -5,7 +5,7 @@ local PLUGIN = PLUGIN
 if timer.Exists("chartimer") then return end
 timer.Create("chartimer", 30, 0, function()
     for _,v in pairs(player.GetAll()) do
-    char = v:getChar()
+    local char = v:getChar()
     if !char then return end
         if (!char:getData("chartime") or char:getData("chartime") == nil)  then
             char:setData("chartime", 30)
