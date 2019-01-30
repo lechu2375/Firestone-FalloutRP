0d918acd1a488783390d76cab9a971e6e6d8f2db
Oskar <oskanio1231@gmail.com>
2019-01-30 01:27:46 +0100

    dodano rangi oraz frakcje do table
    bos, legion cezara i rnk

--- gamemodes/falloutrp/plugins/faction_ranks/core/sh_ranks.lua
+++ gamemodes/falloutrp/plugins/faction_ranks/core/sh_ranks.lua
@@ -1,1 +1,60 @@
 local PLUGIN = PLUGIN
+
+PLUGIN.Ranks = {
+    ["Bractwo Stali"] = {
+        "Nowicjusz",
+        "Kadet",
+        "Rycerz",
+        "Rycerz Sierżant",
+        "Rycerz Kapitan",
+        "Starszy Rycerz",
+        "Paladyn",
+        "Starszy Paladyn",
+        "Gwieździsty Paladyn",
+        "Wartownik",
+        "Starszy",
+        "Nowicjusz Skryba",
+        "Skryba",
+        "Starszy Skryba",
+        "Wysoki Skryba"
+    },
+
+    ["Legion Cezara"] = {
+        "Rekrut",
+        "Weteran",
+        "Zwiadowca",
+        "Decanus",
+        "Centurion",
+        "Legat",
+        "Cezar",
+        "Furmentarii",
+        "Vexillarius",
+        "Pretorianin",
+        "Kowal",
+        "Instruktor"
+    },
+
+    ["Republika Nowej Kalifornii"] = {
+        "Szeregowy",
+        "Starszy Szeregowy",
+        "Kapral",
+        "Sierżant",
+        "Starszy Sierżant",
+        "Sierżant Sztabowy",
+        "Sierżant Major",
+        "Młodszy Porucznik",
+        "Porucznik",
+        "Kapitan",
+        "Major",
+        "Podpułkownik",
+        "Pułkownik",
+        "Brygadier",
+        "Major",
+        "Generał Dywizji",
+        "Generał",
+        "Rekrut",
+        "Strażnik",
+        "Strażnik Weteran",
+        "Chief"
+    }
+}
