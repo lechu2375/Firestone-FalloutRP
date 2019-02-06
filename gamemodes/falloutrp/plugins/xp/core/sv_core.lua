7f2336a770d666c5a599d6072ad989f42d88f36c
Oskar <oskanio1231@gmail.com>
2019-02-06 20:03:35 +0100

    dodano timer na dawanie XP co godzinę
    oprócz tego dodano PLAYER:Notify() po otrzymaniu XP.

--- gamemodes/falloutrp/plugins/xp/core/sv_core.lua
+++ gamemodes/falloutrp/plugins/xp/core/sv_core.lua
@@ -3,26 +3,40 @@ if !SERVER then return end
 local PLUGIN = PLUGIN 
 local PLAYER = FindMetaTable("Player")
 
+-- How many XP should players receive per hour?
+PLUGIN.HourlyBonus = 100
+
 function PLAYER:SetXP(intNumber)
     local char = self:getChar()
 
     char:setData("xp", intNumber)
+    self:Notify("Twoje XP aktualnie wynosi "..intNumber)
 end
 
-function PLAYER:AddXP(intAmount)
+function PLAYER:AddXP(intAmount, strReason)
     local char = self:getChar()
 
     char:setData("xp", char:getData("xp") + intAmount)
+    self:Notify("Otrzymałeś "..intAmount.." XP za "..strReason)
 end
 
 function PLAYER:RemoveXP(intAmount)
     local char = self:getChar()
 
     char:setData("xp", char:getData("xp") - intAmount)
+    self:Notify("Straciłeś "..intAmount.." XP!")
 end
 
 function PLAYER:GetXP()
     local char = self:getChar()
 
     return char:getData("xp")
+end
+
+function PLUGIN:InitPostEntity()
+    timer.Create("Firestone.DailyXP", 3600, 0, function()
+        for _, v in pairs(player.GetAll()) do
+            v:AddXP(PLUGIN.HourlyBonus, "przeżycie godziny na pustkowiu.")
+        end
+    end)
 end
