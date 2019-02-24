31caeeb3a7ebb87ab6af2df39ce0325d70be7f7c
SaturdaysHeroes <saturdaysheroes.business@gmail.com>
2019-02-24 01:24:53 +0100

    dodano funkcję na sprawdzanie czy gracz ma status prime
    PLAYER:IsPrime() który returnuje true/false.

--- gamemodes/falloutrp/plugins/prime/sh_plugin.lua
+++ gamemodes/falloutrp/plugins/prime/sh_plugin.lua
@@ -3,8 +3,18 @@ local PLUGIN = PLUGIN
 PLUGIN.name = "Prime Module"
 PLUGIN.author = "SaturdaysHeroes"
 
+local PLAYER = FindMetaTable("Player")
+
 function PLUGIN:GetMaxPlayerCharacter(client)
     if table.HasValue({"prime", "early_supporter"}, client:GetNWInt("usergroup")) then 
         return nut.config.get("maxChars") + 1
+    end
+end
+
+function PLAYER:IsPrime()
+    if table.HasValue({"prime", "early_supporter"}, self:GetNWInt("usergroup")) then 
+        return true
+    else
+        return false 
     end
 end
