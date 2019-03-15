7b25478edcc4dbe1dec4f29c89b68a937c014a10
Lechu2375 <adamos2375@gmail.com>
2019-03-15 13:25:39 +0100

    usunąłem tabele
    ale już naprawione

--- gamemodes/falloutrp/plugins/damageresist/sv_plugin.lua
+++ gamemodes/falloutrp/plugins/damageresist/sv_plugin.lua
@@ -1,4 +1,6 @@
 local PLUGIN = PLUGIN
+local damagetype =
+{DMG_BULLET,DMG_SLASH,DMG_CLUB,DMG_AIRBOAT,DMG_BUCKSHOT,DMG_SNIPER}
 
 function PLUGIN:EntityTakeDamage(target,dmg)
     if  target:IsPlayer()  then 
