4a23c929c4b6ef7c31aac9ea8f83b8e1a1e36132
Lechu2375 <adamos2375@gmail.com>
2019-03-15 11:41:51 +0100

    update szybki
    Na lekcji push, nie został przetestowany.

--- gamemodes/falloutrp/plugins/damageresist/sv_plugin.lua
+++ gamemodes/falloutrp/plugins/damageresist/sv_plugin.lua
@@ -1,13 +1,19 @@
 local PLUGIN = PLUGIN
-local damagetype =
-{DMG_BULLET,DMG_SLASH,DMG_CLUB,DMG_AIRBOAT,DMG_BUCKSHOT,DMG_SNIPER}
 
 function PLUGIN:EntityTakeDamage(target,dmg)
     if  target:IsPlayer()  then 
         print(dmg:GetDamageType())
         if table.HasValue(damagetype, dmg:GetDamageType()) then
-            target:Notify("Właśnie dostałeś DEMEJDŻ", 3)
+            local char = target:getChar()
+			local dt = char:getData("dt") or 0
+			local dr = char:getData("dr") or 0
+				if (dmg:GetDamage()>dt) then
+					dmg:SetDamage(dmg:GetDamage*dr)
+				end
+				else if (dmg:GetDamage()<dt) then 
+					dmg:SetDamage(0)
+					dmg:SetDamageForce(dmg:GetDamageForce()+dmg:GetDamageForce()*0.2)
+				end
         end
     end
 end
-
