7e920f095a64d1b47275fb4c9fcbe51f3da3a295
Adam <adamos2375@gmail.com>
2019-04-14 19:42:37 +0200

    Niestety nieprzetestowany
    Dodałem dynamiczną tabelę budowaną razem z ładowaniem postaci tak aby rangi dla każdej postaci ładowały się dla każdego.
    Dostęp do daty postaci przez innego gracza jest bardzo trudny, taka tabela jest lepsza.

--- gamemodes/falloutrp/plugins/system_frakcji/sh_plugin.lua
+++ gamemodes/falloutrp/plugins/system_frakcji/sh_plugin.lua
@@ -4,3 +4,58 @@ PLUGIN.desc = "Rozwija funkcjonalność frakcji."
 local chuj = PLUGIN.folder
 nut.util.include("sv_frakcje.lua")
 nut.util.includeDir(chuj.."/frakcje")
+
+TABELA_RANG = {}
+
+function SetRank(char,rangaID)
+    local frakcja = char:getFaction() 
+    char:setData("ranga",nut.faction.indices[frakcja].rangi[rangaID],false,player.GetAll())
+    char:getPlayer():Notify("Twoja ranga została zmieniona na: "..GetRank(char))
+end
+
+function GetRank(char)
+	if char then
+		return char:getData("ranga") or TABELA_RANG[char:getID()]
+	end
+end
+
+
+function PLUGIN:DrawCharInfo(client, character, info)
+    local frakcja = client:getChar():getFaction()
+    local kolor_frakcji = nut.faction.indices[frakcja].color
+    if (nut.faction.indices[frakcja].name ~= "Mieszkaniec Pustkowi") then  
+         info[#info + 1] = {GetRank(client:getChar()), kolor_frakcji}
+    end
+end
+
+
+nut.command.add("jakaranga", {
+	adminOnly = true,
+	syntax = "<nazwa typa>",
+	onRun = function(client, arguments)
+		local target = nut.command.findPlayer(client, arguments[1])
+		if(IsValid(target) and target:getChar()) then
+			client:PrintMessage(HUD_PRINTTALK,"Ranga postaci "..target:getChar():getName().." to: "..target:getChar():getData("ranga"))
+		end
+	end
+})
+
+nut.command.add("awansuj", {
+	adminOnly = true,
+	syntax = "<komu> <numer rangi>",
+	onRun = function(client, arguments)
+		if client:getChar() then
+			local target = nut.command.findPlayer(client, arguments[1])
+			local char = target:getChar()
+			local frakcja = char:getFaction()
+			local id = tonumber(arguments[2])
+			local nowa_ranga = nut.faction.indices[frakcja].rangi[id]
+			client:Notify("Awansowałeś postać: "..char:getName().." na: "..nowa_ranga)
+			SetRank(char,id)
+		end
+	end
+})
+
+
+
+
--- gamemodes/falloutrp/plugins/system_frakcji/sv_frakcje.lua
+++ gamemodes/falloutrp/plugins/system_frakcji/sv_frakcje.lua
@@ -1,15 +1,27 @@
+print("SV FRAKCJE LOADED :)")
+
+TABELA_RANG = {}
+print("testowe printowanie tabeli z rangami...")
+PrintTable(TABELA_RANG)
+
 function PLUGIN:OnCharCreated(client, id) 
     local frakcja = id:getFaction()
     local spis_frakcji = nut.faction.indices
     if (nut.faction.indices[frakcja].name ~= "Mieszkaniec Pustkowi") then
-        print("cos innego niz mieszkaniec pozdro")
         SetRank(id,1)
     end
 end
 
 function SetRank(char,rangaID)
-    local frakcja = char:getFaction()
-    local spis_frakcji = nut.faction.indices 
-    char:setData("ranga",nut.faction.indices[frakcja].rangi[rangaID] )
-    char:getPlayer():Notify("Twoja ranga została zmieniona na: "..char:getData("ranga"))
+    local frakcja = char:getFaction() 
+    char:setData("ranga",nut.faction.indices[frakcja].rangi[rangaID],false,player.GetAll())
+    char:getPlayer():Notify("Twoja ranga została zmieniona na: "..GetRank(char))
+    local charid = char:getID()
+    TABELA_RANG[charid] = GetRank(char)
 end
+
+function PLUGIN:CharacterLoaded(id)
+    TABELA_RANG[id] = GetRank(id)
+end
+
+
