cc73ba939d44ea2e8f8a657f42edd1c2d17b1d28
Adam <adamos2375@gmail.com>
2019-05-07 15:25:22 +0200

    Update pluginu na nieśmiertelniki
    Ulepszono zapisywanie do pliku całej tabeli, dodano hooka od tworzenia postaci. Plugin kompletny, muszę tylko zrobić item nieśmiertelnika.

--- gamemodes/falloutrp/plugins/dogtags/sv_plugin.lua
+++ gamemodes/falloutrp/plugins/dogtags/sv_plugin.lua
@@ -2,43 +2,47 @@ if !file.Exists("dogtags.txt", "DATA") then --Jesli nie ma pliku to go robi i od
 	file.Append("dogtags.txt") 
 	local tabl = {}
 	file.Write("dogtags.txt", util.TableToJSON(tabl))  
+    print("DogTags: Created missing dogtags file for storing table.")
 end
 
-local allowed_factions = {FACTION_RNK,FACTION_BS} --w pliku frakcji musi byc na przykład FACTION_BS = FACTION.index
-                                                    --Dla tych frakcji będą generowane nieśmiertelniki
-dogtagsTable = util.JSONToTable(file.Read( "dogtags.txt", "DATA" ))  
+local allowed_factions = {
+    [FACTION_RNK]=true,
+    [FACTION_BS]=true
+} --w pliku frakcji musi byc na przykład FACTION_BS = FACTION.index
+--Dla tych frakcji będą generowane nieśmiertelniki
 
-if allowed_factions then
-    for k,v in pairs(allowed_factions) do
-        if !dogtagsTable.V then
-            dogtagsTable.V = {}
-        end
-    end
-end
+dogtagsTable = util.JSONToTable(file.Read( "dogtags.txt", "DATA" ))  --Czytamy plik, JSONA konwertujemy tabele
 
 function getDogtagnNum()
+    local number = math.random(342, 2912)
     while dogtagsTable[number] do 
-	    number = math.random(1, 6)
+	    number = math.random(342, 2912)
 	    if !dogtagsTable[number] then 
-	        return number
+	        break
 	    end
 	end
+    return number
 end
 
 function dogtagsSave()
 	file.Write("dogtags.txt", util.TableToJSON(dogtagsTable))
-    dogtagsTable = util.JSONToTable(file.Read( "dogdags.txt", "DATA" )) 
+    print("DogTags: table saved to file.")
 end
 
+--[[function dogtagsForce(char,number)
 
---[[function PLUGIN:OnCharCreated( ply, char )
-    local faction = char:getFaction()
-    if table.HasValue(allowed_factions, faction) then
-        local dogtagnumber = getDogtagnNum()
-        char:setData("dogtag",dogtagnumber) 
-        local id = char:getID
-        dogtagsTable.faction[id] = dogtagnumber
-        print(char:getData("dogtag"))
-    end
 end]]--
 
+
+function PLUGIN:OnCharCreated( client, id )
+    local faction = id:getFaction()
+    if allowed_factions[faction] then
+        local dogtagnumber = getDogtagnNum()
+        id:setData("dogtag",dogtagnumber) 
+        dogtagsTable[dogtagnumber] = id:getID() --KEY w tabeli to numer niesmiertelnika, a wartość to ID postaci
+        print("Przypisano numer nieśmiertelnika: "..dogtagnumber.." dla postaci "..id:getName())
+        dogtagsSave()
+        PrintTable(util.JSONToTable(file.Read( "dogtags.txt", "DATA" )))
+    end
+end
+print("DogTags: Plugin loaded.")
