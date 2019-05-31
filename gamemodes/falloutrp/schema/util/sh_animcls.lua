3093f51d5dfa05f198a30ab2bb2686d5e5cc8e63
Adam <adamos2375@gmail.com>
2019-05-31 14:32:09 +0200

    Naprawa większości tpose.

---
+++ gamemodes/falloutrp/schema/util/sh_animcls.lua
@@ -0,0 +1,30 @@
+nut.anim.setModelClass("models/dominator/fallout/armors/t-45d.mdl", "player")
+--[[player]]
+nut.anim.setModelClass("models/gonzo/falloutpriestredux/falloutpriestredux.mdl", "player")
+nut.anim.setModelClass("models/hub/fallout/armors/knight.mdl", "player")
+nut.anim.setModelClass("models/hub/fallout/armors/paladin.mdl", "player")
+nut.anim.setModelClass("models/hub/fallout/armors/starpaladin.mdl", "player")--Power Armors end
+--Legion
+nut.anim.setModelClass("models/dizcordum/caesar.mdl", "player")
+nut.anim.setModelClass("models/dizcordum/centurion.mdl", "player")
+nut.anim.setModelClass("models/dizcordum/praetorian.mdl", "player")
+--zjebane
+nut.anim.setModelClass("models/frix/cnr/aassasinnew.mdl", "player") 
+nut.anim.setModelClass("models/frix/cnr/explorernew.mdl", "player") 
+nut.anim.setModelClass("models/frix/cnr/primenew22222.mdl", "player")
+nut.anim.setModelClass("models/frix/cnr/recruitnew.mdl", "player")
+nut.anim.setModelClass("models/frix/cnr/veteran1new.mdl", "player")
+nut.anim.setModelClass("models/frix/cnr/vexnew2.mdl", "player")
+nut.anim.setModelClass("models/frix/cnr/warlordnew.mdl", "player")
+--zjebane end 
+nut.anim.setModelClass("models/visualitygaming/fallout/prewar3_male_01.mdl", "player")
+nut.anim.setModelClass("models/visualitygaming/fallout/prewar3_male_02.mdl", "player")
+--RNK 
+nut.anim.setModelClass("models/kryptonite/powerarmor/powerarmor.mdl", "player")
+nut.anim.setModelClass("models/visualitygaming/playermodel/desertranger_female_helmet.mdl", "overwatch") -- zjebane
+nut.anim.setModelClass("models/visualitygaming/playermodel/rangercasual_female_01.mdl", "citizen_female") -- zjebane az do 07
+--
+
+
+
+
