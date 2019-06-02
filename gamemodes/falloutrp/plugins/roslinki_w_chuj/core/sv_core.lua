40db07583cce8d8d8e1cf7a7b298426773e7ef36
Adam <adamos2375@gmail.com>
2019-06-02 22:40:43 +0200

    Roslinki update
    spawn, sprawdzanie itp fajne rzeczy

--- gamemodes/falloutrp/plugins/roslinki_w_chuj/core/sv_core.lua
+++ gamemodes/falloutrp/plugins/roslinki_w_chuj/core/sv_core.lua
@@ -3,36 +3,42 @@ if !SERVER then return end
 Firestone = Firestone || {}
 Firestone.Plants = Firestone.Plants || {}
 
-Firestone.Plants.Time = 1 -- Co ile rośliny się pojawiają?
-Firestone.Plants.Amount = 2 -- Ile roślin się wtedy pojawia?
+Firestone.Plants.Time = 5 -- Co ile rośliny się pojawiają?
+Firestone.Plants.Amount = 1 -- Ile roślin się wtedy pojawia?
 Firestone.Plants.Pos = {
     [ 1 ] = {
-        vector = Vector( 6076.931152, 3760.338867, 74.693886 ),
-        ent = "ent"
-    }
+        pos = Vector( -2686.730469, 9130.890625, 344.425507 ),
+        id = "dynia"
+    },
     [ 2 ] = {
-        vector = Vector( 6076.931152, 3760.338867, 74.693886 ),
-        ent = "ent"
-    }
+        pos = Vector( -2686.730469, 9130.890625, 344.425507 ),
+        id = "kukurydza"
+    },
     [ 3 ] = {
-        vector = Vector( 6076.931152, 3760.338867, 74.693886 ),
-        ent = "ent"
+        pos = Vector( -2686.730469, 9130.890625, 344.425507 ),
+        id = "papryczka"
     }
 }
 
-Firestone.Plants.Spawned = {}
+
+local function plantSpawn(_,ent)
+    ent:SetCollisionGroup(10)
+end
+
 timer.Create( "Firestone.Plants.Spawn", Firestone.Plants.Time, 0, function()
     for i = 0, Firestone.Plants.Amount do
-        -- Wylosuj vector
-        local rand = math.random( 1, #Firestone.Plants.Pos )
-        -- Jeżeli vector się nie powtarza to koontynuuj
-        if !Firestone.Plants.Spawned[ rand ] then
-            -- A teraz dodaj nowy vector do tabeli
-            Firestone.Plants.Spawned[ #Firestone.Plants.Spawned + 1 ] = Firestone.Plants.Pos[ rand ].vector
-            -- Utwórz krzak i ustaw jego pozycję
-            local plant = ents.Create( "fs_plant" )
-            plant:SetPos( Firestone.Plants.Pos[ rand ].vector )
-            plant:Spawn()
+        local random = math.random(1, #Firestone.Plants.Pos)
+        local x = Firestone.Plants.Pos[random]
+        local check = ents.FindInBox(x:Add(10,10,10), x:Add(-10,-10,-10))
+        for k,v in pairs(check) do
+            if v:GetClass() == "nut_item" then
+                print("Coś tam jest, return.")
+                return
+            end
         end
+        nut.item.spawn(Firestone.Plants.Pos[random].id, Firestone.Plants.Pos[random].pos, plantSpawn)
     end
 end )
+
+
+
