be9404b16222e11e37f954625161ffb3f642b5fb
Adam <adamos2375@gmail.com>
2019-06-02 14:55:09 +0200

    Update bestiariusza
    Dodano translator nazw klas NPC.

--- gamemodes/falloutrp/plugins/bestiariusz/core/cl_core.lua
+++ gamemodes/falloutrp/plugins/bestiariusz/core/cl_core.lua
@@ -1,9 +1,9 @@
 bestiariusz = {}
+bestiariusz.list = {}
 if  not file.Exists("bestiariusz.txt", "DATA") then
     file.Append("bestiariusz.txt") 
 	file.Write("bestiariusz.txt", util.TableToJSON({}))  
     print("Stworzono brakujący plik bestiariusza. Ścieżka: DATA/bestiariusz.txt")
-    bestiariusz.list = {}
 else
     bestiariusz.list = util.JSONToTable(file.Read("bestiariusz.txt", "DATA"))
 end
@@ -11,12 +11,12 @@ end
 net.Receive("FS:BestiariuszADD", function ()
     local npc = net.ReadString()
     bestiariusz.add(npc)
-    PrintTable(bestiariusz.list)
 end)
 
 bestiariusz.translate = {
     ["npc_monk"] = "Łysy dziadek",
-    ["npc_crow"] = "Wrona"
+    ["npc_crow"] = "Wrona",
+    ["npc_metropolice"] = "policjant"
 }
 
 function bestiariusz.add(npc)
@@ -28,11 +28,24 @@ function bestiariusz.add(npc)
 end
 
 function bestiariusz.save()
-    file.Write("dogtags.txt", util.TableToJSON(bestiariusz.list)) 
+    file.Write("bestiariusz.txt", util.TableToJSON(bestiariusz.list)) 
+end
+
+function bestiariusz.getTranslatedList()
+local tlist = {}
+    for k,v in pairs(bestiariusz.list) do
+        if bestiariusz.translate[k] then 
+            tlist[bestiariusz.translate[k]]= v
+        else
+            tlist[k] = v
+        end
+    end
+    return tlist
+
 end
 
 timer.Create("bestiariusz.update", 5, 0, function ()
     bestiariusz.save()
-)
+end)
 
 return bestiariusz
