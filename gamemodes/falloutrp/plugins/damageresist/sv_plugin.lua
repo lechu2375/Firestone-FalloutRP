f70c42f7f699a387a0e5732630fd169e0f007600
Adam <adamos2375@gmail.com>
2019-06-09 20:18:57 +0200

    Update damage resist pluginu
    Lepszy kod, zmiana mechaniki redukcji obrażeń.

--- gamemodes/falloutrp/plugins/damageresist/sv_plugin.lua
+++ gamemodes/falloutrp/plugins/damageresist/sv_plugin.lua
@@ -1,36 +1,56 @@
 local PLUGIN = PLUGIN
 local damagetype = 
-{DMG_BULLET, --2 To są enumy w komentarzu.
-DMG_SLASH, --4
-DMG_CLUB, --128
-DMG_AIRBOAT, --33554432
-DMG_BUCKSHOT, --536870912
-DMG_SNIPER} --1073741824
+{
+[DMG_BULLET] = true, --2
+[DMG_SLASH]= true, --4
+[DMG_CLUB]= true, --128
+[DMG_AIRBOAT]= true, --33554432
+[DMG_BUCKSHOT]= true, --536870912
+[DMG_SNIPER]= true} --1073741824
+
+function PLUGIN:GetDr(client)
+	local dr = 0.00
+	local char = client:getChar()
+	local items = char:getInv():getItems()
+    for k, v in pairs(items) do
+        if v.reduction  and v:getData("equip") then
+            dr = dr + v.reduction
+        end
+    end
+	return dr 
+end
+
+function PLUGIN:GetDt(client)
+	local dt = 0
+	local char = client:getChar()
+	local items = char:getInv():getItems()
+    for k, v in pairs(items) do
+        if v.dt  and v:getData("equip") then
+            dt = dt + v.dt
+        end
+    end
+	return dt
+end
+
 
 function PLUGIN:EntityTakeDamage(target,dmg)
-    if  target:IsPlayer() and table.HasValue( damagetype, dmg:GetDamageType() )  then -- szybki check
+    if  target:IsPlayer() and damagetype[dmg:GetDamageType()]   then -- szybki check
 		local attacker = dmg:GetAttacker()
-		local dr = target:getChar():getData("dr") or 0
-		local dt = target:getChar():getData("dt") or 0
-		target:PrintMessage(HUD_PRINTTALK, "-------\ndmg Przed redukcją:"..dmg:GetDamage().."\n dmgtype:"..dmg:GetDamageType().."\ndt:"..dt.."\ndr:"..dr.."\n" )
-        if table.HasValue( damagetype, dmg:GetDamageType() ) and ( attacker:IsNPC() or attacker:IsPlayer() ) then
-        local char = target:getChar()
-				local damagetreshold = char:getData("dt") 
-				local damageresist = char:getData("dr") 
-
-				if (dmg:GetDamage()>=damagetreshold and ( attacker:IsNPC() or attacker:IsPlayer() )) then
-					dmg:SetDamage(dmg:GetDamage()-damagetreshold)
-					target:PrintMessage(HUD_PRINTTALK, "dmg po odjęciu:"..dmg:GetDamage())
-					--[[Dałem tu tyle ifów ponieważ coś mi się rypało w nich i czytało dmg z damage systemu nawet
-			    	może ktoś naprawi, na razie to działa chociaż względem optymalizacji, wiem, że jest to złe rozwiązanie.]]--
-				else if (dmg:GetDamage()<damagetreshold and ( attacker:IsNPC() or attacker:IsPlayer() )) then 
-					dmg:SetDamage(math.random(0, ( dmg:GetDamage() - ( dmg:GetDamage()*damageresist ) )))
-					target:PrintMessage(HUD_PRINTTALK, "dmg po redukcji:"..dmg:GetDamage())
+		local dr = hook.Run("GetDr",target) or 0
+		local dt = hook.Run("GetDt",target) or 0
+		target:PrintMessage(HUD_PRINTTALK, "-------\ndmg Przed redukcją:"..dmg:GetDamage().."\ndt:"..dt.."\ndr:"..dr.."\n" )
+        if damagetype[dmg:GetDamageType()] and ( attacker:IsNPC() or attacker:IsPlayer() ) then
+				local damagetreshold = dt 
+				local damageresist = dr
+				dmg:SetDamage(dmg:GetDamage()*(damageresist/100))
+				if (dmg:GetDamage()-damagetreshold)<0 then
+					dmg:SetDamage(math.random(0, 3))
 					target:EmitSound(("physics/metal/metal_solid_impact_bullet"..math.random(1,4)..".wav"))
-				end 
-
-			end
-			
+					target:PrintMessage(HUD_PRINTTALK, "dmg 0:"..dmg:GetDamage())
+				else 
+					dmg:SetDamage(dmg:GetDamage()-damagetreshold)
+					target:PrintMessage(HUD_PRINTTALK, "dmg po redukcjidt:"..dmg:GetDamage())
+				end	
 		end
-    end
+    end 
 end
