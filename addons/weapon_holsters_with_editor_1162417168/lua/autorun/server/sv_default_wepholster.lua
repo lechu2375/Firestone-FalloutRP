911e39158dde84a2d4941b602a0f39203e6bc916
Adam <adamos2375@gmail.com>
2019-06-10 15:45:35 +0200

    Holstery na bronie
    Na razie dodałem wszystkie bronie palne.

---
+++ addons/weapon_holsters_with_editor_1162417168/lua/autorun/server/sv_default_wepholster.lua
@@ -0,0 +1,71 @@
+--[[
+   __  ___        __      __                                
+  /  |/  /__ ____/ /__   / /  __ __                         
+ / /|_/ / _ `/ _  / -_) / _ \/ // /                         
+/_/  /_/\_,_/\_,_/\__/ /_.__/\_, /                          
+   ___       __             /___/         ___           __  
+  / _ \___  / /_ _____ ___ / /____ ____  / _ \__ ______/ /__
+ / ___/ _ \/ / // / -_|_-</ __/ -_) __/ / // / // / __/  '_/
+/_/   \___/_/\_, /\__/___/\__/\__/_/   /____/\_,_/\__/_/\_\ 
+            /___/                                           
+https://steamcommunity.com/profiles/76561198057599363
+]]
+WepHolster.defData = {}
+----------------------------------------------------------------
+----------------------------------------------------------------
+-- Half-Life 2 Weapons
+----------------------------------------------------------------
+WepHolster.defData["weapon_pistol"] = {}
+WepHolster.defData["weapon_pistol"].Model = "models/weapons/W_pistol.mdl"
+WepHolster.defData["weapon_pistol"].Bone = "ValveBiped.Bip01_Pelvis"
+WepHolster.defData["weapon_pistol"].BoneOffset = {Vector(0, -8, 0), Angle(0, 90, 0)}
+----------------------------------------------------------------
+WepHolster.defData["weapon_357"] = {}
+WepHolster.defData["weapon_357"].Model = "models/weapons/W_357.mdl"
+WepHolster.defData["weapon_357"].Bone = "ValveBiped.Bip01_Pelvis"
+WepHolster.defData["weapon_357"].BoneOffset = {Vector(-5, 8, 0), Angle(0, 270, 0)}
+----------------------------------------------------------------
+WepHolster.defData["weapon_frag"] = {}
+WepHolster.defData["weapon_frag"].Model = "models/Items/grenadeAmmo.mdl"
+WepHolster.defData["weapon_frag"].Bone = "ValveBiped.Bip01_Pelvis"
+WepHolster.defData["weapon_frag"].BoneOffset = {Vector(3, -5, 6), Angle(-95, 0, 0)}
+----------------------------------------------------------------
+WepHolster.defData["weapon_slam"] = {}
+WepHolster.defData["weapon_slam"].Model = "models/weapons/w_slam.mdl"
+WepHolster.defData["weapon_slam"].Bone = "ValveBiped.Bip01_Spine2"
+WepHolster.defData["weapon_slam"].BoneOffset = {Vector(-9, 0, -7), Angle(270, 90, -25)}
+----------------------------------------------------------------
+WepHolster.defData["weapon_crowbar"] = {}
+WepHolster.defData["weapon_crowbar"].Model = "models/weapons/w_crowbar.mdl"
+WepHolster.defData["weapon_crowbar"].Bone = "ValveBiped.Bip01_Spine1"
+WepHolster.defData["weapon_crowbar"].BoneOffset = {Vector(3, 0, 0), Angle(0, 0, 45)}
+----------------------------------------------------------------
+WepHolster.defData["weapon_stunstick"] = {}
+WepHolster.defData["weapon_stunstick"].Model = "models/weapons/W_stunbaton.mdl"
+WepHolster.defData["weapon_stunstick"].Bone = "ValveBiped.Bip01_Spine1"
+WepHolster.defData["weapon_stunstick"].BoneOffset = {Vector(3, 0, 0), Angle(0, 0, -45)}
+----------------------------------------------------------------
+WepHolster.defData["weapon_shotgun"] = {}
+WepHolster.defData["weapon_shotgun"].Model = "models/weapons/W_shotgun.mdl"
+WepHolster.defData["weapon_shotgun"].Bone = "ValveBiped.Bip01_R_Clavicle"
+WepHolster.defData["weapon_shotgun"].BoneOffset = {Vector(10, 5, 2), Angle(0, 90, 0)}
+----------------------------------------------------------------
+WepHolster.defData["weapon_rpg"] = {}
+WepHolster.defData["weapon_rpg"].Model = "models/weapons/w_rocket_launcher.mdl"
+WepHolster.defData["weapon_rpg"].Bone = "ValveBiped.Bip01_L_Clavicle"
+WepHolster.defData["weapon_rpg"].BoneOffset = {Vector(-16, 5, 0), Angle(90, 90, 90)}
+----------------------------------------------------------------
+WepHolster.defData["weapon_smg1"] = {}
+WepHolster.defData["weapon_smg1"].Model = "models/weapons/w_smg1.mdl"
+WepHolster.defData["weapon_smg1"].Bone = "ValveBiped.Bip01_Spine1"
+WepHolster.defData["weapon_smg1"].BoneOffset = {Vector(5, 0, -5), Angle(0, 0, 230)}
+----------------------------------------------------------------
+WepHolster.defData["weapon_ar2"] = {}
+WepHolster.defData["weapon_ar2"].Model = "models/weapons/W_irifle.mdl"
+WepHolster.defData["weapon_ar2"].Bone = "ValveBiped.Bip01_R_Clavicle"
+WepHolster.defData["weapon_ar2"].BoneOffset = {Vector(-5, 0, 7), Angle(0, 270, 0)}
+----------------------------------------------------------------
+WepHolster.defData["weapon_crossbow"] = {}
+WepHolster.defData["weapon_crossbow"].Model = "models/weapons/W_crossbow.mdl"
+WepHolster.defData["weapon_crossbow"].Bone = "ValveBiped.Bip01_L_Clavicle"
+WepHolster.defData["weapon_crossbow"].BoneOffset = {Vector(0, 5, -5), Angle(180, 90, 0)}
