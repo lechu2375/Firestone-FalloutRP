cfd8a66ab4c5987e8e8b90a09f39cdeccbc7646c
Adam <adamos2375@gmail.com>
2019-06-12 20:28:27 +0200

    Noo super spawner addon
    juz nie na nutscripta tylko haha
    -Dodano plik wczytujący resztę rdzenia skryptu
    -Rozplanowanie tabeli od lokacji oraz pojedyńczego spawnu

---
+++ addons/fs_npc_spawner/lua/autorun/server/fs_npcloader.lua
@@ -0,0 +1,29 @@
+include("fnscore/fns_spawner.lua")
+include("fnscore/fns_locations.lua")
+include("fnscore/fns_singlespawns.lua")
+
+Firestone = Firestone or {}
+Firestone.FNS = Firestone.FNS or {}
+Firestone.FNS.Locations = Firestone.FNS.Locations or {}
+Firestone.FNS.SS= Firestone.FNS.SS or {}
+FNS = Firestone.FNS
+--[[
+Miejscówki ={
+["Nazwa_miejscówki"] = { --Nazwa spawnpointa
+    ["npctable"] = {
+        [1] = klasa_npc --każdy npc wpisany
+        [2] = klasa_npc
+    }
+    ["postable"] = {
+        [1] = pozycja --pozycja, pierwszy wpis odpowiada pierwszemu wpisowi z npc tabeli.
+        [2] = pozycja
+    }
+    ["radius"] = promień --promień kuli w której będzie skrypt szukał graczy
+    ["time"] = sekundy --Co ile sekund ma nastąpić sprawdzenie czy npc żyją, jeśli nie to je respi
+
+
+}} 
+
+
+["NPC"] = pos
+]]
---
+++ addons/fs_npc_spawner/lua/fnscore/fns_spawner.lua
@@ -0,0 +1,13 @@
+
+
+function FNS.CheckLocation(stringLocation)
+    if Firestone.FNS.Locations[stringLocation] then 
+    end
+end
+
+function FNS.CheckSS(stringLocation)
+    if Firestone.FNS.SS[stringLocation] then 
+    end
+end
+
+--Respimy npc, nw stringa z lokacją i jego indeksem z tabeli lokacji
