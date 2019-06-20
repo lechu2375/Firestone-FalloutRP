d07443c9823fe5e66ae711cefa3c596446262607
Adam <adamos2375@gmail.com>
2019-06-20 21:12:08 +0200

    Update obsługi pojedyńczych spawnów
    no i git wszystko śmiga

--- addons/fs_npc_spawner/lua/fnscore/fns_spawner.lua
+++ addons/fs_npc_spawner/lua/fnscore/fns_spawner.lua
@@ -49,21 +49,40 @@ end
 
 function FNS.CheckSS(stringUniqeName)
     local ss = Firestone.FNS.SS[stringUniqeName]
-    if ss then
+    if ss then   
         if ss.spawned then return end
-        local npc = ents.Create(ss.nc)
-        local pos = ss.pos
-        local npcinfo = list.Get("NPC")
-        npc:SetPos(pos)
-        npc:Spawn() 
-        ss.spawned = true
-        npc:SetVar("FNSSUname",stringUniqeName)
-        if npcinfo[ss.nc] then
-            if npcinfo[ss.nc].Weapons then
-                npc:Give(table.Random(npcinfo[v].Weapons))
+        if ss.fibv then --jeśli jest tabela z wektorami do find in box
+            if ss.fibv[1] and ss.fibv[2] then --wtedy sprawdzamy czy ktoś z graczy jest w równoległoboku
+                local entsTable = ents.FindInBox(ss.fibv[1], ss.fibv[2]) 
+                for k,v in pairs(entsTable) do
+                    if v:IsPlayer() then
+                        print("[FNS LOG][Box]Can't spawn npc in "..stringUniqeName.." location, someone is there.")
+                    return end
+                end
+            end      
+        elseif ss.radius then
+            local entsTable = ents.FindInSphere(ss.pos, ss.radius)
+            for k,v in pairs(entsTable) do
+                if v:IsPlayer() then
+                    print("[FNS LOG][Sphere] Can't spawn npc in "..stringUniqeName.." location, someone is there.")
+                return end
             end
+        else
+            print("[FNS LOG] Find in box vectors does not exists same as radius! Skipping entities check.")
         end
-        print("[FNS LOG] Spawned "..ss.nc.." at",pos,"with",npc:GetActiveWeapon())
+            local npc = ents.Create(ss.nc)
+            local pos = ss.pos
+            local npcinfo = list.Get("NPC")
+            npc:SetPos(pos)
+            npc:Spawn() 
+            ss.spawned = true
+            npc:SetVar("FNSSUname",stringUniqeName)
+            if npcinfo[ss.nc] then
+                if npcinfo[ss.nc].Weapons then
+                    npc:Give(table.Random(npcinfo[v].Weapons))
+                end
+            end
+            print("[FNS LOG] Spawned "..ss.nc.." at",pos,"with",npc:GetActiveWeapon())
     end
 end
 
