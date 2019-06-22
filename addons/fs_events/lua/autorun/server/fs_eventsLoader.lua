12ba9cdfcdaf584ba7a8a2c3cbd0ba8c65f3bcbf
Adam <adamos2375@gmail.com>
2019-06-23 00:02:16 +0200

    Update system ueventów
    Funkcja losująca event + timer wywołujący funkcje

--- addons/fs_events/lua/autorun/server/fs_eventsLoader.lua
+++ addons/fs_events/lua/autorun/server/fs_eventsLoader.lua
@@ -1,11 +1,38 @@
 Firestone = Firestone or {}
+Firestone.FSE = Firestone.FSE or {}
 FSE = Firestone.FSE or {}
 FSE.EventsTable = FSE.EventsTable or {}
-
+FSE.Time = 10 --w sekundach co ile ma byc evencior odpalany
+util.AddNetworkString("FSEventSoundPlay")
 include("fse_core/events.lua")
-print("FSE load")
+IncludeCS("fse_core/fse_client.lua")
+print("[FSE LOG] Core Loaded")
 
-timer.Simple(7, function ()
-    print("FSE timerek")
-    FSE.EventsTable.testowy.func()
+
+
+
+function FSE.RunRandomEvent()
+    local event = table.Random(FSE.EventsTable)
+    if event.func then
+        event.func()
+    end
+    if event.msg then
+        net.Start("FSEventSoundPlay")
+        print("broadkast")
+        net.Broadcast()
+        for k,v in pairs(player.GetAll()) do
+                --v:Notify(event.msg)
+        end
+    end
+
+
+end
+
+timer.Create("FSE.TIMER", FSE.Time, 0, FSE.RunRandomEvent)
+
+timer.Create("FSE.SafetyTimer", FSE.Time+1, 0, function () --jak sie timer wywalił przez funkcje z eventu wtedy na nowo go odpala
+    if not timer.Exists("FSE.TIMER") then
+        timer.Create("FSE.TIMER", FSE.Time, 0, FSE.RunRandomEvent)
+    end
 end)
+
---
+++ addons/fs_events/lua/fse_core/fse_client.lua
@@ -0,0 +1,4 @@
+net.Receive("FSEventSoundPlay", function(
+    print("przyszło")
+    surface.PlaySound("fs_ui/ui_wildwasteland.wav")
+end)
