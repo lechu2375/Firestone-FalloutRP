9ca6ede9ccdd1a13902bb4f768525629047616ae
Adam <adamos2375@gmail.com>
2019-06-27 15:07:09 +0200

    Update sh_pacoutfit_goggle.lua

---
+++ gamemodes/falloutrp/schema/items/pacoutfit/sh_pacoutfit_goggle.lua
@@ -0,0 +1,31 @@
+ITEM.name = "Czapka wędrownego handlarza"
+ITEM.desc = "Z daszkiem - chroni przed słońcem."
+ITEM.model = "models/visualitygaming/fallout/prop/prop/rovingtrader_hat.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.outfitCategory = "hat"
+ITEM.pacData = {
+["children"] = {
+	[1] = {
+		["children"] = {
+		},
+		["self"] = {
+			["BoneMerge"] = true,
+			["ClassName"] = "model",
+			["UniqueID"] = "848882256",
+			["Model"] = "models/visualitygaming/fallout/prop/prop/rovingtrader_hat.mdl",
+		},
+	},
+},
+["self"] = {
+	["EditorExpand"] = true,
+	["UniqueID"] = "1449590088",
+	["ClassName"] = "group",
+},
+}
+
+ITEM.iconCam = {
+	pos = Vector(333.25375366211, 278, 205),
+	ang = Angle(25, 220, 0),
+	fov = 1.7647058823529
+}
