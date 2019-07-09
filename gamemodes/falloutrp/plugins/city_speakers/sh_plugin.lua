60cb574c389a1710f8e9df94e12657c1d0565ad5
Adam <adamos2375@gmail.com>
2019-07-09 12:28:38 +0200

    plugin na głośniki w mieście
    funkcja zwracająca wszystkie głośniki, funkcja włączania i wyłączania wszystkich na raz

---
+++ gamemodes/falloutrp/plugins/city_speakers/sh_plugin.lua
@@ -0,0 +1,4 @@
+PLUGIN.name = "Głośniki w mieście"
+PLUGIN.author = "Lechu2375"
+nut.util.include("sv_core.lua")
+
---
+++ gamemodes/falloutrp/plugins/city_speakers/sv_core.lua
@@ -0,0 +1,29 @@
+FS.speakers = {}
+speakers = FS.speakers{}
+function speakers.getAll()
+    local speakers = {}
+    for k,v in pairs(ents.GetAll()) do
+        if v:IsValid() and (v:GetClass()=="radio_receiver") then
+            speakers[#speakers+1] = v
+        end
+    end
+    return speakers
+end
+
+function speakers.TurnOff() 
+    for k,v in pairs(speakers.getAll()) do
+        timer.Simple(math.random(1, 3), function ()
+            v:EmitSound("ambient/energy/newspark0"..math.random(1,9)..".wav")
+        end)
+        v.dt.On = false
+    end
+end
+
+function speakers.TurnOn() 
+    for k,v in pairs(speakers.getAll()) do
+        timer.Simple(math.random(1, 3), function ()
+            v:EmitSound("optinvfallout/obj_switch_button_b_pass.wav")
+        end)
+        v.dt.On = true
+    end
+end
