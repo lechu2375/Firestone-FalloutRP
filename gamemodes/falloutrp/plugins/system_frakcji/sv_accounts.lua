6458e52a0bdc0033350e24473397d06b1f96fe5e
Adam <adamos2375@gmail.com>
2019-07-12 22:39:34 +0200

    Update sv_accounts.lua

--- gamemodes/falloutrp/plugins/system_frakcji/sv_accounts.lua
+++ gamemodes/falloutrp/plugins/system_frakcji/sv_accounts.lua
@@ -1,4 +1,6 @@
-Firestone.FactionsVault = Firestone.FactionsVault or util.JSONToTable(file.Read("sv_vault.txt")) or {}
+if not file.Exists("sv_vault.txt","DATA") then file.Append("sv_vault.txt","{}") end
+Firestone.FactionsVault = {}
+Firestone.FactionsVault.FactionData = Firestone.FactionsVault.FactionData or util.JSONToTable(file.Read("sv_vault.txt")) or {}
 Firestone.FactionsVault.Allowed = {
     ["bs"] = true,
     ["legion"] = true,
@@ -6,7 +8,20 @@ Firestone.FactionsVault.Allowed = {
 }
 
 for k,v in pairs(nut.faction.indices) do
-    if (Firestone.FactionsVault.Allowed[v.uniqueID]) and (not Firestone.FactionsVault[v.uniqueID]) then
-        Firestone.FactionsVault[v.uniqueID] = 0
+    if (Firestone.FactionsVault.Allowed[v.uniqueID]) and (not Firestone.FactionsVault.FactionData[v.uniqueID]) then
+        Firestone.FactionsVault.FactionData[v.uniqueID] = {} 
+        Firestone.FactionsVault.FactionData[v.uniqueID]["money"] = 0
     end
+end
+
+function Firestone.FactionsVault.Save()
+    file.Write("sv_vault.txt", util.TableToJSON(Firestone.FactionsVault.FactionData))
+end
+
+function Firestone.FactionsVault.Add(char,amount)
+    if not char then return end
+    if not char:hasMoney(amount) then return end
+    local faction = nut.faction.indices[char:getFaction()][uniqueID]
+    if not Firestone.FactionsVault.Allowed.faction then return end
+
 end
