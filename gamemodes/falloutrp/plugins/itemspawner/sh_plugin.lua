50c20beca684ad827da1964d41464eb7c66e752f
Adam <adamos2375@gmail.com>
2019-08-06 21:35:45 +0200

    item spawner muj
    tabela, wczytanie tabeli z pliku, zapis tabeli do pliku, dodanie nowego spawna po tabeli/stringu samym pozdrawiam

---
+++ gamemodes/falloutrp/plugins/itemspawner/sh_plugin.lua
@@ -0,0 +1,5 @@
+PLUGIN.name = "Item Spawner"
+PLUGIN.description = "Dodanie randomowego spawnera itemów"
+PLUGIN.author = "Lechu2375"
+
+nut.util.include("sv_spawner.lua")
---
+++ gamemodes/falloutrp/plugins/itemspawner/sv_spawner.lua
@@ -0,0 +1,58 @@
+FS = FS or {}
+FS.ItemSpawner = FS.ItemSpawner or {}
+
+function FS.ItemSpawner.LoadTable()
+    if not IsValid(FS.ItemSpawner.SpawnTable) or table.IsEmpty(FS.ItemSpawner.SpawnTable) then
+        if not file.Exists("FSitemspawner.txt", "DATA") then
+            file.Append("FSitemspawner.txt","{}")
+        end        
+        FS.ItemSpawner.SpawnTable = util.JSONToTable(file.Read("FSitemspawner.txt"))
+    end
+end
+
+function FS.ItemSpawner.SaveTable()
+    if IsValid(FS.ItemSpawner.SpawnTable) and not table.IsEmpty(FS.ItemSpawner.SpawnTable) then
+        file.Write("FSitemspawner.txt", util.TableToJSON(FS.ItemSpawner.SpawnTable))
+    end
+end
+
+function FS.ItemSpawner.AddSpawn(data)
+    if istable(data) then --jesli chcemy dodać tabele
+        if not IsValid(data.item) then return false end --jesli nie ma sprecyzowanego to nara
+        local newTable = {} --nowa tabela do konwersji danych bo tak mi sie podoba
+        newTable.item = tostring(data.item) -- w razie czego konwersja do stringa
+
+        if isvector(data.pos) then --jak mamy sprecyzowany spawn wtedy wrzucamy go do tabeli nowej
+            newTable.pos = data.pos 
+        else
+            print("[Item Spawner] wrong vector, adding item to random spawn...") --no jak nie to pzodro
+        end
+
+        if IsValid(data.itemData) and istable(data.itemData) then --jesli mamy tabele z itemdata n oto wrzucamy jom
+            newTable.itemData = data.itemData
+        end
+
+        FS.ItemSpawner.SpawnTable[#FS.ItemSpawner.SpawnTable+1] = newTable --dodajemy do tabeli
+        FS.ItemSpawner.SaveTable() --fast attack save
+
+    elseif isstring(data) then --jak to po prostu string
+        if IsValid(nut.item.list[data]) then --czy jest itemem
+            FS.ItemSpawner.SpawnTable[#FS.ItemSpawner.SpawnTable+1] = data --dodanie do tabeli
+            FS.ItemSpawner.SaveTable() --zapis
+        end
+    else
+        return false --no jak nie ma ani kurwa tabeli ani stringa to NARA DO JASNEJ kurwy
+    end
+end
+
+--[[
+data = {
+    item = stringItem
+    pos = Vector(0, 0, 0)
+    itemData = {}
+}
+]]--
+
+
+
+
